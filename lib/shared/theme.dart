796756d971a9839a2b2543801681d250c2a24a81
sonex333 <sona@crowde.co>
2021-10-19 00:24:03 +0700

    initial commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = const Color(0xff5C40CC);
+Color kBackgroundColor = const Color(0xffFAFAFA);
+Color kBlackColor = const Color(0xff1F1449);
+Color kWhiteColor = const Color(0xffFFFFFF);
+Color kGreyColor = const Color(0xff9698A9);
+Color kGreenColor = const Color(0xFF0EC3AE);
+Color kRedColor = const Color(0xffEB70A5);
+Color kInactiveColor = const Color(0xffDBD7EC);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
